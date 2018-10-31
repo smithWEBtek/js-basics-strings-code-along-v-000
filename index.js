2e0676a0712dd15bf4aa449833bfb2ac870ed085
smithwebtek <brad@smithwebtek.com>
2018-10-31 14:54:14 -0400

    Update head

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let currentUser = "ned"
+const currentUser = "ned"
 const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
 const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
 const shortGreeting = "Welcome, " + currentUser[0] + "!"
